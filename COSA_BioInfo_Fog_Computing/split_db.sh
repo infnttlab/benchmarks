396c35c31bb49d0fe50b37c40a29d5f3d214ab43
Elena <elena.corni@libero.it>
2018-03-13 17:50:47 +0100

    Update split_db.sh

--- COSA_BioInfo_Fog_Computing/split_db.sh
+++ COSA_BioInfo_Fog_Computing/split_db.sh
@@ -2,19 +2,20 @@
 
 if [[ ( $1 == "--help" ) || ( $1 == "-h" ) ]]
 then
-        echo -e "\\nUsage: $0  [n-max-core]  [n-file]  [dir]\\n"
+        echo -e "\\nUsage: $0  [dest-path]   [n-max-core]  [n-file]  [db-name]\\n"
         exit 0
 fi
 
-if [[ ( -z "$3" ) ]]
+if [[ ( -z "$4" ) ]]
 then
-        echo -e "\\nUsage: $0  [n-max-core]  [n-file]  [dir]\\n"
+        echo -e "\\nUsage: $0  [dest-path]   [n-max-core]  [n-file]  [db-name]\\n"
         exit 1
 fi
 
-maxcore=$1
-nfile=$2
-dir=${3%/}
+path=${1%/}
+maxcore=$2
+nfile=$3
+dir=${4%/}
 
 list_file=($(ls -1 ${dir} | head -n ${nfile}))
 
@@ -24,7 +25,7 @@ nth=1
 while [ "${nth}" -le "${maxcore}" ]; do
         for num in $(seq $nth); do
                 file_in=$((nfile / nth))
-                dest="dataset4test/th${nth}/${dir}_${num}_${file_in}/"
+                dest="${path}/splitDB/split${nth}/${dir}_${num}_${file_in}/"
                 mkdir -p "${dest}"
 
                 low=$(((num - 1) * file_in))
@@ -32,7 +33,7 @@ while [ "${nth}" -le "${maxcore}" ]; do
 
                 for i in $(seq $low $high); do
                         if [ -f "$PWD/${dir}/${list_file[$i]}" ]; then
-                                ln -sf "$PWD/${dir}/${list_file[$i]}" "${dest}/${list_file[$i]}"
+                                cp  "$PWD/${dir}/${list_file[$i]}" "${dest}/${list_file[$i]}"
                         fi
                 done
         done
