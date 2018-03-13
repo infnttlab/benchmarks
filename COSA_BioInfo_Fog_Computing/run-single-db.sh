2370466c10b09de12c1077f7cf4e1ac2da7de940
Elena <elena.corni@libero.it>
2018-03-13 18:05:53 +0100

    Update run-single-db.sh

--- COSA_BioInfo_Fog_Computing/run-single-db.sh
+++ COSA_BioInfo_Fog_Computing/run-single-db.sh
@@ -2,29 +2,30 @@
 
 if [[ ( $1 == "--help" ) || ( $1 == "-h" ) ]]
 then
-        echo -e "\\nUsage: $0  [n-max-core]  [n-file-tot]  [dir]\\n"
+        echo -e "\\nUsage: $0  [n-max-core]  [n-file-tot]  [db-name]  [db-path]"
+        echo "Where [db-path] is the full path before 'splitDB/' dir."
+        echo -e "i.e: /mnt/avoton/fog-computing/data/test/splitDB/ ---> [db-path] = /mnt/avoton/fog-computing/data/test/\\n"
         exit 0
 fi
 
 if [[ ( -z "$3" ) ]]
 then
-        echo -e "\\nUsage: $0  [n-max-core]  [n-file-tot]  [dir]\\n"
+        echo -e "\\nUsage: $0  [n-max-core]  [n-file-tot]  [db-name]  [db-path]"
+        echo "Where [db-path] is the path before 'splitDB/' dir."
+        echo -e "i.e: /mnt/avoton/fog-computing/data/test/splitDB/ ---> [db-path] = /mnt/avoton/fog-computing/data/test/\\n"
         exit 1
 fi
 
 maxcore=$1
 nfile=$2
 dir=${3%/}
-
-#list_file=($(ls -1 ${dir} | head -n ${nfile}))
-
-#TODO assicurarsi che list_file sia lungo proprio nfile
+pathDB=${4%/}
 
 nth=1
 while [ "${nth}" -le "${maxcore}" ]; do
         for num in $(seq $nth); do
                 file_in=$((nfile / nth))
-                dest="/mnt/avoton/fog/data/dataset4test/th${nth}/${dir}_${num}_${file_in}/"
+                dest="${pathDB}/splitDB/split${nth}/${dir}_${num}_${file_in}/"
                 cp -rL ${dest} ../
         done
 
