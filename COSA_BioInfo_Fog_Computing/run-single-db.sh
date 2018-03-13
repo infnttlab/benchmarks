cebed3dbf9fdf39833a29615f4d747ffffd57f4e
Elena <elena.corni@libero.it>
2018-03-13 17:43:48 +0100

    Create run-single-db.sh

---
+++ COSA_BioInfo_Fog_Computing/run-single-db.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [[ ( $1 == "--help" ) || ( $1 == "-h" ) ]]
+then
+        echo -e "\\nUsage: $0  [n-max-core]  [n-file-tot]  [dir]\\n"
+        exit 0
+fi
+
+if [[ ( -z "$3" ) ]]
+then
+        echo -e "\\nUsage: $0  [n-max-core]  [n-file-tot]  [dir]\\n"
+        exit 1
+fi
+
+maxcore=$1
+nfile=$2
+dir=${3%/}
+
+#list_file=($(ls -1 ${dir} | head -n ${nfile}))
+
+#TODO assicurarsi che list_file sia lungo proprio nfile
+
+nth=1
+while [ "${nth}" -le "${maxcore}" ]; do
+        for num in $(seq $nth); do
+                file_in=$((nfile / nth))
+                dest="/mnt/avoton/fog/data/dataset4test/th${nth}/${dir}_${num}_${file_in}/"
+                cp -rL ${dest} ../
+        done
+
+        ./running_in_the_fog_MCORE_flag.sh  ${nth}  ${dir}  ${file_in}
+
+        nth=$((nth * 2))
+done
+
+rm -rf  ../${dir}_*
