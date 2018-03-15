4d648ad8e7e354a4d9e35d17f3dd8f42a3d5a16a
lumorganti <lucia.morganti@cnaf.infn.it>
2018-03-15 14:54:59 +0100

    Delete run_all_db.sh

--- COSA_BioInfo_Fog_Computing/run_all_db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#db: Ecoli_2D Equal_v5_2D Equal_v6_2D Maeru_2D Pfluor_2D Rare_v6_2D Selong_2D Staggered_2D
-
-for db in Ecoli_2D Equal_v5_2D Equal_v6_2D Maeru_2D Pfluor_2D Rare_v6_2D Selong_2D Staggered_2D
-do
-        ./run-single-db.sh 8 32 ${db}
-done
